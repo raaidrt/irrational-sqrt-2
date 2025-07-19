d49079bdc429c0bcfc0c3f01ee945cb282574e5c
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2025-07-19 14:02:57 -0700

    Comments

--- Sqrt2Irrational/Basic.lean
+++ Sqrt2Irrational/Basic.lean
@@ -11,68 +11,77 @@ theorem sqrt_2_is_irrational : Irrational √2 := by
   intro h
   push_neg at h
   obtain ⟨a, b, hab⟩ : ∃ a b : ℤ, √2 = a / b := h
-  obtain ⟨a'', ha''_eq⟩ : ∃a'' : ℚ, a'' = a := by norm_num
-  obtain ⟨b'', hb''_eq⟩ : ∃b'' : ℚ, b'' = b := by norm_num
-  have hrational_equiv: a'' / b'' = a / b := by simp [ha''_eq, hb''_eq]
-  by_cases hb_zero : (b : ℝ) = 0
-  · rw [hb_zero] at hab
+
+  have hb_zero : b ≠ 0 := by
+    apply byContradiction
+    intro hb_non_zero
+    push_neg at hb_non_zero
+    rw [hb_non_zero] at hab
     have h₄ : a / 0 = 0 := by simp
     have h₅ : √2 = 0 := by
       calc
         √2 = a / 0 := by simp [hab]
-         _ = 0 := by simp
+        _ = 0 := by simp
     norm_num at h₅
-  · push_neg at hb_zero
-    have hb_zero : b ≠ 0 := by
-      intro h_b_eq_zero
-      refine hb_zero ?_
-      simp [h_b_eq_zero]
-    obtain ⟨a', b', h_coprime, h_eq, hb'_non_zero⟩ := by
-      apply exists_reduced_rational_for_rational a b hb_zero
-    have h_eq_ℝ : (a : ℝ) / b = a' / b' := by
-      norm_cast
-      rify [h_eq]
+
+  obtain ⟨a', b', h_coprime, h_eq, hb'_non_zero⟩ := by
+    apply exists_reduced_rational_for_rational a b hb_zero
+
+  -- #check (h_coprime : IsCoprime a' b')
+  -- #check (h_eq : (a : ℚ) / b = a' / b')
+  -- #check (hb'_non_zero : b' ≠ 0)
+
+  have h_eq_ℝ : (a : ℝ) / b = a' / b' := by
+    norm_cast
+    rify [h_eq]
+    calc
+      (a : ℝ) / b = Rat.cast ((a : ℚ) / b) := by norm_cast
+                _ = Rat.cast ((a' : ℚ) / b') := by simp [h_eq]
+                _ = a' / b' := by norm_num
+  have : √2 = (a' : ℝ) / b' := by
+    calc
+      √2 = a / b := by apply hab
+        _ = a' / b' := by simp [h_eq_ℝ]
+
+  -- transform our goal to proving absurdity of a claim on a reduced rational
+  -- and it's numerator a' and denominator b'
+  have hab : 2 * b'^2 = a'^2 := by
+    apply transform_sq_eq_rational a' b' hb'_non_zero this
+
+
+  rcases Int.even_or_odd a' with h_even | h_odd
+  · -- a' is even
+    let ⟨kₐ, hkₐ⟩ := h_even
+    have hkₐ : a' = 2 * kₐ := by calc
+      a' = kₐ + kₐ := hkₐ
+      _ = 2 * kₐ := by ring
+    have : 2 * b'^2 = 4 * kₐ^2 := by
       calc
-        (a : ℝ) / b = Rat.cast ((a : ℚ) / b) := by norm_cast
-                  _ = Rat.cast ((a' : ℚ) / b') := by simp [h_eq]
-                  _ = a' / b' := by norm_num
-    have : √2 = (a' : ℝ) / b' := by
+        2 * b'^2 = a'^2 := by exact hab
+              _ = (2 * kₐ)^2 := by congr 1
+              _ = 4 * kₐ^2 := by ring
+    have hkb : 2 * b'^2 = 2 * (2 * kₐ^2) := by
       calc
-        √2 = a / b := by apply hab
-         _ = a' / b' := by simp [h_eq_ℝ]
-    have hab : 2 * b'^2 = a'^2 :=
-        by apply transform_sq_eq_rational a' b' hb'_non_zero this
-    rcases Int.even_or_odd a' with h_even | h_odd
-    · let ⟨kₐ, hkₐ⟩ := h_even
-      have hkₐ : a' = 2 * kₐ := by calc
-        a' = kₐ + kₐ := hkₐ
-        _ = 2 * kₐ := by ring
-      have : 2 * b'^2 = 4 * kₐ^2 := by
-        calc
-          2 * b'^2 = a'^2 := by exact hab
-                _ = (2 * kₐ)^2 := by congr 1
-                _ = 4 * kₐ^2 := by ring
-      have hkb : 2 * b'^2 = 2 * (2 * kₐ^2) := by
-        calc
-          2 * b'^2 = 4 * kₐ^2 := by exact this
-                _ = 2 * (2 * kₐ^2) := by ring
-      have hkb : b'^2 = 2 * kₐ^2 := by exact mul_left_cancel₀ (by norm_num) hkb
-      have hkb : 2 ∣ b'^2 := by simp [hkb]
-      have hkb : 2 ∣ b' := by apply Prime.dvd_of_dvd_pow prime_two hkb
-      have hb_even : Even b' := by apply even_iff_two_dvd.mpr hkb
-      have : ¬IsCoprime a' b' := not_coprime_if_both_even a' b' h_even hb_even
-      absurd this
-      exact h_coprime
-    · have ⟨kₐ, h_kₐ⟩ : ∃kₐ, a' = 2 * kₐ + 1  := by apply h_odd
-      have h_eq' : 2 * b' ^ 2 = 2 * (2 * kₐ ^ 2 + 2 * kₐ) + 1 := by calc
-        2 * b'^2 = a'^2 := by apply hab
-              _ = (2 * kₐ + 1)^2 := by rw [h_kₐ]
-              _ = 4 * kₐ ^ 2 + 4 * kₐ + 1 := by ring
-              _ = 2 * (2 * kₐ ^ 2 + 2 * kₐ) + 1 := by ring
-      have h1 : Even (2 * b'^2) := ⟨b'^2, by ring⟩
-      have h2 : Odd (2 * (2 * kₐ^2 + 2 * kₐ) + 1) := ⟨2 * kₐ^2 + 2 * kₐ, by ring⟩
-      rw [←h_eq'] at h2
-      have h2 : ¬Even (2 * b'^2) := by
-        apply Int.not_even_iff_odd.mpr h2
-      absurd h2
-      exact h1
+        2 * b'^2 = 4 * kₐ^2 := by exact this
+              _ = 2 * (2 * kₐ^2) := by ring
+    -- extract that a' and b' are not coprime since both a' and b' are even
+    have hkb : b'^2 = 2 * kₐ^2 := by exact mul_left_cancel₀ (by norm_num) hkb
+    have hkb : 2 ∣ b'^2 := by simp [hkb]
+    have hkb : 2 ∣ b' := by apply Prime.dvd_of_dvd_pow prime_two hkb
+    have hb_even : Even b' := by apply even_iff_two_dvd.mpr hkb
+    have : ¬IsCoprime a' b' := not_coprime_if_both_even a' b' h_even hb_even
+    contradiction
+  · -- a' is odd
+    have ⟨kₐ, h_kₐ⟩ : ∃kₐ, a' = 2 * kₐ + 1  := by apply h_odd
+    -- extract contradiction since 2 * b'^2 is even but 2 * a'^2 + 1 is odd
+    have h_eq' : 2 * b' ^ 2 = 2 * (2 * kₐ ^ 2 + 2 * kₐ) + 1 := by calc
+      2 * b'^2 = a'^2 := by apply hab
+            _ = (2 * kₐ + 1)^2 := by rw [h_kₐ]
+            _ = 4 * kₐ ^ 2 + 4 * kₐ + 1 := by ring
+            _ = 2 * (2 * kₐ ^ 2 + 2 * kₐ) + 1 := by ring
+    have h1 : Even (2 * b'^2) := ⟨b'^2, by ring⟩
+    have h2 : Odd (2 * (2 * kₐ^2 + 2 * kₐ) + 1) := ⟨2 * kₐ^2 + 2 * kₐ, by ring⟩
+    rw [←h_eq'] at h2
+    have h2 : ¬Even (2 * b'^2) := by
+      apply Int.not_even_iff_odd.mpr h2
+    contradiction
--- Sqrt2Irrational/Helpers.lean
+++ Sqrt2Irrational/Helpers.lean
@@ -1,9 +1,9 @@
 import Mathlib
 
 open Real
-open Classical
 open Int
 
+open Classical in
 theorem exists_reduced_rational_for_rational (a b : ℤ) (hb_non_zero : b ≠ 0)
   : ∃a' b' : ℤ, IsCoprime a' b' ∧ ((a : ℚ) / b = a' / b') ∧ b' ≠ 0 := by
   let d : ℕ := a.gcd b
@@ -35,49 +35,19 @@ theorem exists_reduced_rational_for_rational (a b : ℤ) (hb_non_zero : b ≠ 0)
 
 theorem not_two_dvd_implies_odd (a : ℕ) (ha_odd : ¬2 ∣ a) : Odd a := by
   rcases Int.even_or_odd a with h | h
-  · -- Case: a is even
-    -- If a is even, then 2 ∣ a, which contradicts ha_odd
-    apply absurd (even_iff_two_dvd.mp h)
+  · apply absurd (even_iff_two_dvd.mp h)
     norm_cast
-  · -- Case: a is odd
-    -- We already have what we want
-    exact_mod_cast h
-
-/-- lemma prime_two' : Prime 2 := by
-  constructor
-  · -- Prove 2 ≠ 1
-    norm_num
-  · constructor
-    · norm_num -- 2 is non-unit
-    · -- Prove that for all a b, if a * b = 2, then a = 1 or b = 1
-      intro a b hab_dvd_prod
-      apply byContradiction
-      intro h_contra
-      push_neg at h_contra
-      obtain ⟨ha_odd, hb_odd⟩ := h_contra
-      have ha_odd : Odd a := by apply not_two_dvd_implies_odd a ha_odd
-      have hb_odd : Odd b := by apply not_two_dvd_implies_odd b hb_odd
-      obtain ha_mul_b_odd := Nat.odd_mul.mpr ⟨ha_odd, hb_odd⟩
-      obtain ha_mul_b_even := even_iff_two_dvd.mpr hab_dvd_prod
-      have ha_mul_b_not_odd : ¬Odd (a * b) := Nat.not_odd_iff_even.mpr ha_mul_b_even
-      exact ha_mul_b_not_odd ha_mul_b_odd -/
+  · exact_mod_cast h
 
 theorem prime_pow_dvd (b : ℤ) (h : (2 : ℤ) ∣ b ^ 2) : (2 : ℤ) ∣ b := by
   rw [sq] at h
-  -- b^2 = b * b, so 2 | b * b
-  -- Since 2 is prime, 2 | b or 2 | b
   have := Int.prime_two.dvd_mul.mp h
   cases this <;> assumption
 
 lemma transform_sq_eq_rational (a b : ℤ) (hb : b ≠ 0) (h : √2 = a / b) : 2 * b^2 = a^2 := by
-  -- Square both sides of the equation
   have h_sq : (√2)^2 = (a / b)^2 := by rw [h]
-  -- Simplify left side using sqrt_sq
   rw [Real.sq_sqrt (by norm_num : (0 : ℝ) ≤ 2)] at h_sq
-  -- Simplify right side using div_pow
   rw [div_pow] at h_sq
-  -- Now we have 2 = a^2 / b^2
-  -- Multiply both sides by b^2
   have : 2 * b^2 = a^2 := by
     field_simp at h_sq
     norm_cast
